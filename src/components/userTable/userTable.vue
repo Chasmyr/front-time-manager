a17f3eac83a3ec10320b882f31c1e3cd7a890233
evans hulot <hulotevans@gmail.com>
2023-11-03 12:53:23 +0100

    commit before pull

--- src/components/userTable/userTable.vue
+++ src/components/userTable/userTable.vue
@@ -1,54 +1,53 @@
 <script>
-export default {
-  data() {
-    return {
-      user: this.$store.state.currUser
+  import { ApiGet } from "../../utils/api.js";
+  export default {
+    data() {
+      return {
+        users: [],
+        teams: []
+      }
+    },
+    created() {
+      this.getUsers()
+    },
+    methods: {
+      async getUsers() {
+        this.users = await ApiGet('/users')
+      }
+
+      // createTeam() {
+
+      // }
     }
   }
-}
 </script>
 
 <template>
   <div class="flex">
     <div id="users-table" class="mr-4 px-6 py-4 bg-sky-700 rounded-3xl justify-between text-white">
+      <!-- <form>
+          <div class="relative">
+              <input type="search" id="default-search" class="block w-full p-4 text-sm text-black border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search Mockups, Logos..." required>
+              <button type="submit" class="text-black absolute right-2.5 bottom-2.5 font-medium rounded-lg text-sm px-4 py-2">
+                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                </svg>
+              </button>
+          </div>
+      </form> -->
       <table class="min-w-full divide-y divide-gray-200 ">
         <thead>
           <tr>
             <th class="font-bold px-6 py-3 text-left text-xs text-gray-500 uppercase tracking-wider">Username</th>
             <th class="font-bold px-6 py-3 text-left text-xs text-gray-500 uppercase tracking-wider">Email</th>
-            <th class="font-bold px-6 py-3 text-left text-xs text-gray-500 uppercase tracking-wider">Rôle</th>
+            <!-- <th class="font-bold px-6 py-3 text-left text-xs text-gray-500 uppercase tracking-wider">Rôle</th> -->
           </tr>
         </thead>
         <tbody class=" divide-y divide-gray-200">
-          <tr>
+          <tr v-for="user in users" :key="user">
             <td class="px-6 py-4 whitespace-nowrap">{{ user.username }}</td>
             <td class="px-6 py-4 whitespace-nowrap">{{ user.email }}</td>
-            <td class="px-6 py-4 whitespace-nowrap">{{ user.role }}</td>
-          </tr>
-          <tr>
-            <td class="px-6 py-4 whitespace-nowrap">{{ user.username }}</td>
-            <td class="px-6 py-4 whitespace-nowrap">{{ user.email }}</td>
-            <td class="px-6 py-4 whitespace-nowrap">{{ user.role }}</td>
-          </tr>
-          <tr>
-            <td class="px-6 py-4 whitespace-nowrap">{{ user.username }}</td>
-            <td class="px-6 py-4 whitespace-nowrap">{{ user.email }}</td>
-            <td class="px-6 py-4 whitespace-nowrap">{{ user.role }}</td>
-          </tr>
-          <tr>
-            <td class="px-6 py-4 whitespace-nowrap">{{ user.username }}</td>
-            <td class="px-6 py-4 whitespace-nowrap">{{ user.email }}</td>
-            <td class="px-6 py-4 whitespace-nowrap">{{ user.role }}</td>
-          </tr>
-          <tr>
-            <td class="px-6 py-4 whitespace-nowrap">{{ user.username }}</td>
-            <td class="px-6 py-4 whitespace-nowrap">{{ user.email }}</td>
-            <td class="px-6 py-4 whitespace-nowrap">{{ user.role }}</td>
-          </tr>
-          <tr>
-            <td class="px-6 py-4 whitespace-nowrap">{{ user.username }}</td>
-            <td class="px-6 py-4 whitespace-nowrap">{{ user.email }}</td>
-            <td class="px-6 py-4 whitespace-nowrap">{{ user.role }}</td>
+            <!-- <td class="px-6 py-4 whitespace-nowrap">{{ user.role }}</td> -->
           </tr>
         </tbody>
       </table>
@@ -68,7 +67,7 @@ export default {
           </div>
         </div>
       </div>
-      <table class="mt-4 min-w-full divide-y divide-gray-200">
+      <!-- <table class="mt-4 min-w-full divide-y divide-gray-200">
         <thead>
           <tr>
             <th class="font-bold px-6 py-3 text-left text-xs text-gray-500 uppercase tracking-wider">Username</th>
@@ -88,7 +87,7 @@ export default {
             <td class="px-6 py-4 whitespace-nowrap">{{ user.role }}</td>
           </tr>
         </tbody>
-      </table>
+      </table> -->
     </div>
 
   </div>
