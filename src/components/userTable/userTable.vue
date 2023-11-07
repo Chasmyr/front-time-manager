d0d961171a926aa3fcacd3c67c06e5ee2affb5fc
evans hulot <hulotevans@gmail.com>
2023-11-07 09:31:24 +0100

    add TableData

--- src/components/userTable/userTable.vue
+++ src/components/userTable/userTable.vue
@@ -20,7 +20,8 @@ export default {
       ],
       searchField: ["username", "email", "role"],
       searchValue: "",
-      sortBy: "teamsId"
+      sortBy: "teamsId",
+      loading: false
     }
   },
 
@@ -30,7 +31,9 @@ export default {
 
   methods: {
     async getUsers() {
+      this.loading = true
       this.users = await ApiGet('/users')
+      this.loading = false
     },
 
     deleteUser(user) {
@@ -41,24 +44,46 @@ export default {
 </script>
 
 <template>
-  <span>Search user: </span>
-  <input type="text" v-model="searchValue">
-  <Vue3EasyDataTable
-    :headers="header"
-    :items="users"
-    :search-field="searchField"
-    :search-value="searchValue"
-    :sortBy="sortBy"
-  >
-  <template #item-teamsId="team">
-    <select>
-      <option value="">1</option>
-      <option value="">2</option>
-      <option value="">3</option>
-    </select>
-  </template>
-  <template #item-action="user">
-    <button @click="deleteUser(user.username)">Delete</button>
-  </template>
-</Vue3EasyDataTable>
+  <div class="w-full h-62 p-3 bg-clockbg rounded-3xl shadow flex flex-col">
+    <span>Search user: </span>
+    <input type="text" v-model="searchValue">
+    <Vue3EasyDataTable
+      :headers="header"
+      :items="users"
+      :search-field="searchField"
+      :search-value="searchValue"
+      :sortBy="sortBy"
+      :loading="loading"
+    >
+    <template #item-username="item">
+      <div>
+        <button @click="console.log(`go to ${item.username} dashboard`)">{{ item.username }}</button>
+      </div>
+    </template>
+    <template #item-teamsId="item">
+      <div v-if="item.teamsId == null">
+        <p>No team</p>
+      </div>
+      <div v-else>
+        <select>
+          <option>{{ item.teamsId }}</option>
+        </select>
+      </div>
+    </template>
+    <template #item-role="item">
+      <div>
+        <select>
+          <option value="" disabled>{{ item.role }}</option>
+          <option value="">manager</option>
+        </select>
+      </div>
+    </template>
+    <template #item-action="user">
+      <button @click="deleteUser(user.username)">Delete</button>
+    </template>
+    <template #empty-message>
+      <p>No user corresponding</p>
+    </template>
+  </Vue3EasyDataTable>
+  </div>
 </template>
